c664322d67a46c69779526301c7f40ae9fa5e368
arscholtes <alex@ascholtes.me>
2017-06-22 17:56:37 -0400

    woot

--- index.js
+++ index.js
@@ -1,36 +1,61 @@
 const express = require("express");
-const app = express();
 const mustacheExpress = require("mustache-express");
-const bodyParser = require("body-parser");
-const expressValidator = require("express-validator");
+const session = require('express-session');
+const bodyParser = require('body-parser');
+
+const app = express();
 app.engine("mustache", mustacheExpress());
 app.set("view engine", "mustache");
 app.set("views", "./views");
+app.use(express.static("/"));
 app.use(bodyParser.urlencoded({
   extended: true
+}))
+
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitalized: true
 }));
-app.use("views", express.static("/views"));
-app.use(expressValidator());
 
-var views = reg.session.views || {}
 
-const userInfo = {
-  username: "username",
-  password: "password"
-}
+app.use(function(req, res, next) {
+  req.session.users = {
+    Edwin: "bacon1",
+    Jon: "cheese",
+    Josh: "apple"
+  };
 
-const data = {
+  next();//callback
+});
 
-}
+app.get('/', function(req, res, next) {
+  if (req.session.username) {
+    console.log(req.session.username);
+    res.send("Hello " + req.session.username)
+  } else {
+    res.redirect("/login")
+  }
+});
 
-app.get("/", function(req, res) {
-  res.render('login', data);
+app.get("/login", function(req, res, next) {
+  res.render("index");
+})
+
+app.post('/login', function(req, res, next) {
+  if (req.session.users[req.body.username] === req.body.password) {
+    req.session.username = req.body.username;
+  }
+  res.redirect("/")
 });
 
 
-app.post('/',function(req, res) {
-  console.log(req.body);
-  res.redirect('/');
+
+app.post('/', function(req, res, next) {
+  if (req.session.users[req.body.username] === req.body.password) {
+    res.send(!req.session.user)
+  }
 })
 
 
